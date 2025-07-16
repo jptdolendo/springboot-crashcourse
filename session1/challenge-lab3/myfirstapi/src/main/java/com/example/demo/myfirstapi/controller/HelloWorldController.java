3a7f9b43a17f91f51f4147f68b2dba50a9210d4b
Jericho Paolo Dolendo <jerichopaolo.dolendo@gcash.com>
2025-07-16 21:43:26 +0800

    Add challenge lab 3

--- session1/challenge-lab3/myfirstapi/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session1/challenge-lab3/myfirstapi/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
 
@@ -33,5 +38,19 @@ public class HelloWorldController {
             @RequestParam("num1") int number1, @RequestParam("num2") int number2) {
         int sum = number1 + number2;
         return "The sum of " + number1 + " and " + number2 + " is " + sum;
+    }
+
+    @GetMapping("/info")
+    public Map<String, String> getAppInfo() {
+        Map<String, String> info = new HashMap<>();
+        info.put("appName", "MyFirstSpringBootAPI");
+        info.put("version", "1.0.0");
+        info.put("status", "Running");
+        return info;
+    }
+
+    @GetMapping("/features")
+    public List<String> getFeatures() {
+        return Arrays.asList("REST API", "Spring Boot", "Easy Setup", "Fast Development");
     }
 }
