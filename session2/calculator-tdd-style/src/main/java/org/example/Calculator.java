3f06194e75c1871b4a1ef7aaeb5768f8dbd1dcb4
Jahren Hans Dayag <jahrenhans.dayag@gcash.com>
2025-07-18 11:39:03 +0800

    Add calculator class and methods

--- session2/calculator-tdd-style/src/main/java/org/example/Calculator.java
+++ session2/calculator-tdd-style/src/main/java/org/example/Calculator.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Calculator {
+
+    //Addition
     public int add(int numOne, int numTwo) {
         return numOne + numTwo;
     }
@@ -19,5 +21,73 @@ public class Calculator {
 
     public double add(double numOne, double numTwo) {
         return numOne + numTwo;
+    }
+
+    //Subtract
+
+    public int subtract(int numOne, int numTwo) {
+        return numOne - numTwo;
+    }
+
+    public long subtract(long numOne, long numTwo) {
+        return numOne - numTwo;
+    }
+
+    public short subtract(short numOne, short numTwo) {
+        return (short) (numOne - numTwo);
+    }
+
+    public float subtract(float numOne, float numTwo) {
+        return numOne - numTwo;
+    }
+
+    public double subtract(double numOne, double numTwo) {
+        return numOne - numTwo;
+    }
+
+
+    //Multiply
+
+    public int multiply(int numOne, int numTwo) {
+        return numOne * numTwo;
+    }
+
+    public long multiply(long numOne, long numTwo) {
+        return numOne * numTwo;
+    }
+
+    public short multiply(short numOne, short numTwo) {
+        return (short) (numOne * numTwo);
+    }
+
+    public float multiply(float numOne, float numTwo) {
+        return numOne * numTwo;
+    }
+
+    public double multiply(double numOne, double numTwo) {
+        return numOne * numTwo;
+    }
+
+
+    //Divide
+
+    public int divide(int numOne, int numTwo) {
+        return numOne / numTwo;
+    }
+
+    public long divide(long numOne, long numTwo) {
+        return numOne / numTwo;
+    }
+
+    public short divide(short numOne, short numTwo) {
+        return (short) (numOne / numTwo);
+    }
+
+    public float divide(float numOne, float numTwo) {
+        return numOne / numTwo;
+    }
+
+    public double divide(double numOne, double numTwo) {
+        return numOne / numTwo;
     }
 }
